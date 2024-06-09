a47c6a0d213b820c26404c4fca44b99e00e39aee
AGNESGITAU <agnesgitau40@gmail.com>
2024-06-09 14:36:27 +0300

    create new entry in database authors table

--- models/author.py
+++ models/author.py
@@ -1,7 +1,15 @@
+from .connection import get_db_connection
+
 class Author:
     def __init__(self, id, name):
         self.id = id
         self.name = name
+        conn = get_db_connection
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO authors (name) VALUES (?)', (self._name,))
+        self._id = cursor.lastrowid
+        conn.commit()
+        conn.close()
 
     def __repr__(self):
         return f'<Author {self.name}>'
