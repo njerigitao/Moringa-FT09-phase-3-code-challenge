bcad0804e52045f8d175072242f63f120513e221
AGNESGITAU <agnesgitau40@gmail.com>
2024-06-09 17:12:01 +0300

    Use save method in article to connect to the database

--- models/article.py
+++ models/article.py
@@ -1,11 +1,27 @@
+from database.connection import get_db_connection
+from models.author import Author
+from models.magazine import Magazine
 
 class Article:
     def __init__(self, id, title, content, author_id, magazine_id):
-        self.id = id
+        self.id = None
         self.title = title
         self.content = content
         self.author_id = author_id
         self.magazine_id = magazine_id
+        self.save()
+    
+    def save(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO articles (title, content, author_id, magazine_id)
+            VALUES (?, ?, ?, ?)           
+        ''', (self._title, self._content, self._author.id, self._magazine.id))
+        self._id = cursor.lastrowid
+        conn.commit()
+        conn.close()
+    
 
 
     def __repr__(self):
