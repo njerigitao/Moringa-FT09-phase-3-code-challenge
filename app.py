39f97dd11e9e6b2e94649aad4d8750eebb970628
AGNESGITAU <agnesgitau40@gmail.com>
2024-06-09 17:55:37 +0300

    Create an author in app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from database.setup import create_tables
-from database.connection import get_db_connection
+#from database.connection import get_db_connection
 from models.article import Article
 from models.author import Author
 from models.magazine import Magazine
@@ -16,8 +16,8 @@ def main():
     article_content = input("Enter article content: ")
 
     # Connect to the database
-    conn = get_db_connection()
-    cursor = conn.cursor()
+    #conn = get_db_connection()
+    #cursor = conn.cursor()
 
 
     '''
@@ -26,32 +26,34 @@ def main():
     '''
 
     # Create an author
-    cursor.execute('INSERT INTO authors (name) VALUES (?)', (author_name,))
-    author_id = cursor.lastrowid # Use this to fetch the id of the newly created author
+    #cursor.execute('INSERT INTO authors (name) VALUES (?)', (author_name,))
+    #author_id = cursor.lastrowid # Use this to fetch the id of the newly created author
+    author = Author(author_name)
 
     # Create a magazine
-    cursor.execute('INSERT INTO magazines (name, category) VALUES (?,?)', (magazine_name, magazine_category))
-    magazine_id = cursor.lastrowid # Use this to fetch the id of the newly created magazine
+    magazine = Magazine(magazine_name, magazine_category)
+    #cursor.execute('INSERT INTO magazines (name, category) VALUES (?,?)', (magazine_name, magazine_category))
+    #magazine_id = cursor.lastrowid # Use this to fetch the id of the newly created magazine
 
     # Create an article
-    cursor.execute('INSERT INTO articles (title, content, author_id, magazine_id) VALUES (?, ?, ?, ?)',
-                   (article_title, article_content, author_id, magazine_id))
+    #cursor.execute('INSERT INTO articles (title, content, author_id, magazine_id) VALUES (?, ?, ?, ?)',
+                   #(article_title, article_content, author_id, magazine_id))
 
-    conn.commit()
+    #conn.commit()
 
     # Query the database for inserted records. 
     # The following fetch functionality should probably be in their respective models
 
-    cursor.execute('SELECT * FROM magazines')
-    magazines = cursor.fetchall()
+    #cursor.execute('SELECT * FROM magazines')
+    #magazines = cursor.fetchall()
 
-    cursor.execute('SELECT * FROM authors')
-    authors = cursor.fetchall()
+    #cursor.execute('SELECT * FROM authors')
+    #authors = cursor.fetchall()
 
-    cursor.execute('SELECT * FROM articles')
-    articles = cursor.fetchall()
+    #cursor.execute('SELECT * FROM articles')
+    #articles = cursor.fetchall()
 
-    conn.close()
+    #conn.close()
 
     # Display results
     print("\nMagazines:")
@@ -59,8 +61,11 @@ def main():
         print(Magazine(magazine["id"], magazine["name"], magazine["category"]))
 
     print("\nAuthors:")
-    for author in authors:
-        print(Author(author["id"], author["name"]))
+    authors = author.articles()
+    for article in authors:
+        print(article)
+    #for author in authors:
+        #print(Author(author["id"], author["name"]))
 
     print("\nArticles:")
     for article in articles:
